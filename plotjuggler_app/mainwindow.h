7bd312f3959e5285443a9c96315d280e615c9b79
Georg von Zengen <github@vz-ge.org>
2025-11-16 20:48:21 +0100

    UI: add quick button the toggle UTC stamps
    the button toggles whether timestamps are displayed in UTC or in local
    time zone

--- plotjuggler_app/mainwindow.h
+++ plotjuggler_app/mainwindow.h
@@ -323,10 +323,12 @@ private slots:
   void on_buttonReferencePoint_toggled(bool checked);
 
   void on_buttonShowpoint_toggled(bool checked);
+  void on_buttonUseUtc_toggled(bool checked);
 
   void on_buttonDots_toggled(bool checked);
 
 private:
+  bool _use_utc_time;
   QStringList readAllCurvesFromXML(QDomElement root_node);
   void loadAllPlugins(QStringList command_line_plugin_folders);
 };

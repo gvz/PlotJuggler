a59ce3c8944e52606642b94e4993bf365cdbe1fc
Georg von Zengen <github@vz-ge.org>
2026-06-03 07:56:00 +0200

    nix: pre-fetch wasmer tarball to fix sandbox build failure
    download_wasmer.cmake tries to fetch from GitHub at configure time,
    which fails in the nix sandbox. Pre-fetch the tarball as a
    fixed-output derivation and patch the cmake file to use SOURCE_DIR.

--- flake.nix
+++ flake.nix
@@ -20,6 +20,35 @@
           sha256 = "sha256-hGfoU6oK7vh39TRCBTYnlqEsvGLWCsLVRBXh3RDrmnY=";
         };
 
+        wasmer-src =
+          let
+            version = "v7.0.1";
+            base = "https://github.com/wasmerio/wasmer/releases/download/${version}";
+            sources = {
+              "x86_64-linux" = {
+                url = "${base}/wasmer-linux-amd64.tar.gz";
+                sha256 = "10a55885b11eb51b06bb24ff184facde8c2a83c252782a0c04e7a46926630d72";
+              };
+              "aarch64-linux" = {
+                url = "${base}/wasmer-linux-aarch64.tar.gz";
+                sha256 = "21d6968d33defa4a31d878022d261667a8fa8abbfe96007d5f4f28564b7fa372";
+              };
+              "x86_64-darwin" = {
+                url = "${base}/wasmer-darwin-amd64.tar.gz";
+                sha256 = "3a0f44a3aae570b0870d4573fa663c7f0c96a2f9550e38eb22c3be7c77658a1e";
+              };
+              "aarch64-darwin" = {
+                url = "${base}/wasmer-darwin-arm64.tar.gz";
+                sha256 = "3eff017389fb838b0b5af607a4d392edc6039e76343984fcd24307aa027d67ee";
+              };
+            };
+            info = sources.${system};
+          in
+          pkgs.runCommandNoCC "wasmer-src" { } ''
+            mkdir -p $out
+            tar xzf ${pkgs.fetchurl { inherit (info) url sha256; }} -C $out
+          '';
+
         plotjuggler-pkg = pkgs.qt5.mkDerivation {
           pname = "plotjuggler";
           version = "3.13.2";
@@ -31,6 +60,11 @@
             substituteInPlace cmake/find_or_download_data_tamer.cmake \
               --replace-fail "URL" "SOURCE_DIR" \
               --replace-fail "https://github.com/PickNikRobotics/data_tamer/archive/refs/tags/1.0.3.zip" "${data-tamer-src}"
+
+            sed -i \
+              -e 's|URL ''${WASMER_URL}|SOURCE_DIR ${wasmer-src}|' \
+              -e '/URL_HASH SHA256=/d' \
+              cmake/download_wasmer.cmake
 
             rm cmake/find_or_download_fmt.cmake
             rm cmake/find_or_download_lz4.cmake
